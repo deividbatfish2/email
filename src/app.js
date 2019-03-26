9b9739a9d5e48c8cea99fbf5f27bc999b3f13f72
Deivid Teixeira <deivid@contabilizei.com.br>
2019-03-25 22:36:08 -0300

    Finaliza v1, ainda com problemas de concorrência

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const Email = require('./email/Email')
 const sendEmail = require('./email/sendEmail')
 const express = require('express')
 const bodyparser = require('body-parser')
+const dash = require('appmetrics-dash').attach()
 
 const passport = require('passport')
 const {Strategy} = require('passport-http-bearer')
@@ -18,8 +19,14 @@ app.use(bodyparser.json())
 
 app.post('/email', passport.authenticate('bearer', {session: false}), async (req, res) => {
     const email = new Email(req.body)
-    const emailSent = await sendEmail(email).catch(console.error);
-    res.send({emailSent})
+    try {
+        const emailSent = await sendEmail(email)
+        res.send({emailSent})
+    }
+    catch(e) {
+        console.error(e)
+        res.status(500).send({erro: "Unespected error"})
+    }
 })
 
 app.listen(process.env.APP_PORT, () => {console.log(`UP in port ${process.env.APP_PORT}`)})
